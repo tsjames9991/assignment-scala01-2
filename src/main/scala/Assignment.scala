d0aada41cb0d1b5405939c67e60593aa430923eb
Sudeep James Tirkey <sudeep.james@knoldus.in>
2018-01-10 10:09:30 +0530

    Removed all usage of var and warnings

--- src/main/scala/Assignment.scala
+++ src/main/scala/Assignment.scala
@@ -1,47 +1,48 @@
-class Assignment{
-  def showListWithIndex(number_list: List[Int]): Unit ={
-    number_list.foreach( number => println(s"${number_list.indexOf(number)} = ${number}") )
+class Assignment {
+  def showListWithIndex(number_list: List[Int]): Unit = {
+    number_list.foreach(number => println(s"${number_list.indexOf(number)} = $number"))
   }
 
   def showMaximum(number_list: List[Int]): Int = {
     val head = number_list.head
     val tail = number_list.tail
-    if(tail.isEmpty) head
-    else{
+    if (tail.isEmpty) head
+    else {
       val temp = showMaximum(tail)
-      if(head > temp) head else temp
+      if (head > temp) head else temp
     }
   }
 
-  def calculateSumOfRange(n: Int): Int = {
-    if(n == 0)
+  def calculateSumOfDigits(n: Int): Int = {
+    if (n == 0)
       0
     else
-      n + calculateSumOfRange(n - 1)
+      (n % 10) + calculateSumOfDigits(n / 10)
   }
 
-  def calculateProductOfRange(n: Int): Long = {
-    if(n == 1)
+  def calculateProductOfRange(n: Int): Int = {
+    if (n == 1)
       1
     else
       n * calculateProductOfRange(n - 1)
   }
 
   def calculateFibonacci(n: Int): Int = {
-    if(n < 2) n
+    if (n < 2) n
     else calculateFibonacci(n - 1) + calculateFibonacci(n - 2)
   }
 
 }
 
 object Assignment extends App {
-  val obj : Assignment = new Assignment
-  val test_list = List[Int](21,34,89,12,34,51)
-  val input_for_range = 15
-  val input_for_fibonacci = 10
+  val obj: Assignment = new Assignment
+  val test_list = List(21, 34, 89, 12, 34, 51, -10)
+  val input_for_range = 5
+  val input_for_fibonacci = 4
+  val temp = obj.calculateProductOfRange(input_for_range)
   obj.showListWithIndex(test_list)
-  println(s"The Maximum In The List Is ${obj.showMaximum(test_list)}")
-  println(s"The Sum of Range : ${obj.calculateSumOfRange(input_for_range)}")
-  println(s"The Product of Range : ${obj.calculateProductOfRange(input_for_range)}")
-  println(s"The ${input_for_fibonacci}th Fibonacci Number : ${obj.calculateFibonacci(input_for_fibonacci)}")
+//  println(s"The Maximum In The List Is ${obj.showMaximum(test_list)}")
+  println(s"The Sum of Digits : ${obj.calculateSumOfDigits(temp)}")
+//  println(s"The Product of Range : ${obj.calculateProductOfRange(input_for_range)}")
+//  println(s"The ${input_for_fibonacci}th Fibonacci Number : ${obj.calculateFibonacci(input_for_fibonacci)}")
 }
--- target/scala-2.12/classes/prac.sc
+++
@@ -1,8 +0,0 @@
-def calculateProductOfRange(n: Int): Long = {
-  val range = 1 to n
-  var product: Long  = 1
-  for(i <- range) yield { product = product * i }
-  product
-}
-
-calculateProductOfRange(5)
